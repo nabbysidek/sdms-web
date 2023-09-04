8fd9e13fef55a11ed2f227a24ebff2092dde5013
Nur Aina Balqis Mohamad Rosidek <nabby.sidek@gmail.com>
2023-09-04 08:55:39 +0800

    module #1 css and responsive update
    css for the sign-in and sign-up pages have been included

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./components/layout/Layout";
-import SignIn from "./pages/log masuk/SignIn";
-import SignUp from "./pages/daftar masuk/SignUp";
+import SignIn from "./views/auth/SignIn";
+import SignUp from "./views/auth/SignUp";
 import NoPage from "./pages/NoPage";
 import Profile from "./pages/profil/Profil";
 import Dashboard from "./pages/dashboard/Index";
