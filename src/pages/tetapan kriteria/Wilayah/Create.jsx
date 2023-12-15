14e4fe176a56eabe34f2e13d1a266fae9d1dc94d
Nur Aina Balqis Mohamad Rosidek <nabby.sidek@gmail.com>
2023-12-15 11:10:43 +0800

    updated create component for wilayah

--- src/pages/tetapan kriteria/Wilayah/Create.jsx
+++ src/pages/tetapan kriteria/Wilayah/Create.jsx
@@ -8,29 +8,38 @@ function CreateWilayah() {
   // ----------FE----------
   const [showCreateWilayah, setShowCreateWilayah] = useState(false);
 
-  const handleCloseCreateWilayah = () => setShowCreateWilayah(false);
   const handleShowCreateWilayah = () => setShowCreateWilayah(true);
+  const handleCloseCreateWilayah = () => {
+    setShowCreateWilayah(false);
+    reset();
+  };
 
   // Form validation
-  const { control, handleSubmit, formState } = useForm();
-  const { errors } = formState;
+  // Form validation
+  const {
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+  } = useForm();
 
   // Form input
-  const [wilayahInput, setWilayahInput] = useState({
-    namaWilayah: "",
-  });
+  // const [wilayahInput, setWilayahInput] = useState({
+  //   namaWilayah: "",
+  // });
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setWilayahInput({
-      ...wilayahInput,
-      [name]: value,
-    });
-  };
+  // const handleInputChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setWilayahInput({
+  //     ...wilayahInput,
+  //     [name]: value,
+  //   });
+  // };
 
   // ----------BE----------
   // Create wilayah
-  const createWilayah = async () => {
+  const createWilayah = async (wilayahInput) => {
+    // console.log(wilayahInput);
     try {
       const response = await axios.post(
         `http://127.0.0.1:8000/api/tetapan-kriteria/wilayah`,
@@ -38,23 +47,18 @@ function CreateWilayah() {
       );
 
       if (response.status === 200) {
-        Swal.fire({
-          icon: "success",
-          title: "Berjaya",
-          text: response.data.message, // Access the message from the backend response
-        });
+        // Swal.fire({
+        //   icon: "success",
+        //   title: "Berjaya",
+        //   text: response.data.message, // Access the message from the backend response
+        // });
         console.log("Wilayah berjaya ditambah");
         handleCloseCreateWilayah();
       }
     } catch (error) {
-      console.log("Api respond is not as expected");
+      console.log("Wilayah tidak berjaya ditambah");
+      // console.log(error);
     }
-  };
-
-  const onSubmit = (data) => {
-    handleCloseCreateWilayah();
-    // Perform your submit logic here
-    console.log("Form submitted with data:", data);
   };
 
   return (
@@ -73,33 +77,33 @@ function CreateWilayah() {
           <Modal.Title>Tambah Wilayah</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={handleSubmit(createWilayah)} onReset={reset}>
             <Form.Group>
               <Form.Label>Nama Wilayah</Form.Label>
               <Controller
-                name="wilayah"
+                name="namaWilayah"
+                id="namaWilayah"
                 control={control}
+                defaultValue=""
                 rules={{ required: "Wilayah baru diperlukan" }}
-                render={({ field }) => (
-                  <>
-                    <Form.Control
-                      type="text"
-                      placeholder="Wilayah"
-                      {...field}
-                    />
-                    {errors?.wilayah && (
-                      <span className="error-message">
-                        {errors.wilayah.message}
-                      </span>
-                    )}
-                  </>
+                render={({ field: { onChange, value } }) => (
+                  <Form.Control
+                    type="text"
+                    onChange={onChange}
+                    value={value}
+                    placeholder="Masukkan wilayah"
+                    autoFocus
+                  />
                 )}
               />
+              {errors?.wilayah && (
+                <span className="error-message">{errors.wilayah.message}</span>
+              )}
             </Form.Group>
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button className="modalBtn" onClick={handleSubmit(onSubmit)}>
+          <Button className="modalBtn" onClick={handleSubmit(createWilayah)}>
             Tambah Wilayah
           </Button>
         </Modal.Footer>
