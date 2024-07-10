a84d158a16116bb77983d2f278759255818135c3
Nur Aina Balqis Mohamad Rosidek <nabby.sidek@gmail.com>
2024-07-10 11:43:35 +0800

    update repot-individu store

---
+++ src/store/repot-individu-store.js
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+import axiosCustom from "../axios";
+import Swal from "sweetalert2";
+
+const useRepotIndividuStore = create((set, get) => ({
+
+  // search
+  linkClicked: false,
+  setLinkClicked: (value) => set({ linkClicked: value }),
+  validationErrors: null,
+  setValidationErrors: (errors) => set({ validationErrors: errors }),
+  searchResults: null,
+  setSearchResults: (results) => set({ searchResults: results }),
+
+}));
+
+export default useRepotIndividuStore;
