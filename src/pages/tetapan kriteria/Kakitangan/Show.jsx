2de52c26e3f84b3f8f368d9c9c8ca5a1103dab41
Nur Aina Balqis Mohamad Rosidek <nabby.sidek@gmail.com>
2024-07-31 17:02:43 +0800

    test using tanstack

--- src/pages/tetapan kriteria/Kakitangan/Show.jsx
+++ src/pages/tetapan kriteria/Kakitangan/Show.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo, useCallback } from "react";
 import { Table, Button, Row, Container } from "react-bootstrap";
 import CreateKakitangan from "./Create";
 import EditKakitangan from "./Edit";
@@ -7,6 +7,7 @@ import PaginationTable from "../../../components/page layout/PaginationTable";
 import ExportButton from "../../../components/functional buttons/ExportBtn";
 import ImportButton from "../../../components/functional buttons/ImportBtn";
 import useKakitanganStore from "../../../store/kakitangan-store";
+import { useReactTable, getCoreRowModel, flexRender } from "@tanstack/react-table";
 
 function ShowKakitanganList() {
   // initialize store
@@ -21,98 +22,99 @@ function ShowKakitanganList() {
     fetchKakitangans(currentPage);
   }, [currentPage, fetchKakitangans]);
 
+  const data = useMemo(() => kakitangans, [kakitangans]);
 
-  // handle delete of kakitangan
-  const handleDeleteKakitangan = async (kakitanganId) => {
+  const handleDeleteKakitangan = useCallback(async (kakitanganId) => {
     const confirmResult = await showConfirmationDialog();
 
     if (confirmResult.isConfirmed) {
       await deleteKakitangan(kakitanganId);
-      
-      // fetch the total number of items after deletion
-      const updatedKakitangans = await fetchKakitangans(currentPage);
-
-      // If the current page is empty and not the first page, go to the previous page
-      if (updatedKakitangans.length === 0 && currentPage > 1) {
-        const newPage = currentPage - 1;
-        setCurrentPage(newPage);
-        await fetchKakitangans(newPage);
-      }
+      await fetchKakitangans(currentPage);
     }
-  };
+  }, [deleteKakitangan, fetchKakitangans, currentPage]);
 
-  // handles page reload
-  const handleAddSuccess = async () => {
+  const handleAddSuccess = useCallback(async () => {
     await fetchKakitangans(currentPage);
+  }, [fetchKakitangans, currentPage]);
 
-    const totalItemsAfterAdd = totalItems + 1;
-    const newTotalPage = Math.ceil(totalItemsAfterAdd / pageSize);
+  const handleUpdateSuccess = useCallback(async () => {
+    await fetchKakitangans(currentPage);
+  }, [fetchKakitangans, currentPage]);
 
-    if (totalItemsAfterAdd > pageSize * totalPage) {
-      setCurrentPage(newTotalPage);
-      await fetchKakitangans(newTotalPage);
-    } else {
-      setCurrentPage(totalPage);
-      await fetchKakitangans(totalPage);
-    }
-  };
+  const columns = useMemo(() => [
+    {
+      header: "ID",
+      accessorFn: (row, i) => i + 1,
+      id: 'index',
+    },
+    {
+      header: "ID Kakitangan",
+      accessorKey: "idKakitangan",
+    },
+    {
+      header: "Nama Kakitangan",
+      accessorKey: "namaKakitangan",
+    },
+    {
+      header: "Tindakan",
+      cell: ({ row }) => (
+        <div>
+          <EditKakitangan kakitangan={row.original} onUpdateSuccess={handleUpdateSuccess} />
+          <Button
+            onClick={() => handleDeleteKakitangan(row.original.id)}
+            className="delete-btn"
+          >
+            Padam
+          </Button>
+        </div>
+      ),
+    },
+  ], [handleDeleteKakitangan, handleUpdateSuccess]);
+
+  const table = useReactTable({ data, columns, getCoreRowModel: getCoreRowModel() });
 
   return (
-    <>
-      <Container fluid>
-        <div className="table-section">
-          <Row>
-            <div className="col-md-9">
-              <h3 className="table-title">Senarai Kakitangan</h3>
-            </div>
-            <div className="col-md-3">
-              <CreateKakitangan onAddSuccess={handleAddSuccess} />
-            </div>
-          </Row>
-        </div>
-        <hr />
-        <Table responsive>
-          <thead>
-            <tr>
-              <th>Bil</th>
-              <th>ID Kakitangan</th>
-              <th>Nama Kakitangan</th>
-              <th>Tindakan</th>
-            </tr>
-          </thead>
-          <tbody>
-            {kakitangans.length > 0 &&
-              kakitangans.map((kakitangansData, key) => (
-                <tr key={key}>
-                  <td>{(currentPage - 1) * pageSize + key + 1}</td>
-                  <td>{kakitangansData.idKakitangan}</td>
-                  <td>{kakitangansData.namaKakitangan}</td>
-                  <td>
-                    <EditKakitangan kakitangan={kakitangansData} onUpdateSuccess={() => fetchKakitangans(currentPage)} />
-                    <Button
-                      onClick={() => handleDeleteKakitangan(kakitangansData.id)}
-                      className="delete-btn"
-                    >
-                      Padam
-                    </Button>
-                  </td>
-                </tr>
+    <Container fluid>
+      <div className="table-section">
+        <Row>
+          <div className="col-md-9">
+            <h3 className="table-title">Senarai Kakitangan</h3>
+          </div>
+          <div className="col-md-3">
+            <CreateKakitangan onAddSuccess={handleAddSuccess} />
+          </div>
+        </Row>
+      </div>
+      <hr />
+      <Table responsive>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {flexRender(header.column.columnDef.header, header.getContext())}
+                </th>
               ))}
-          </tbody>
-        </Table>
-
-        <PaginationTable
-          currentPage={currentPage}
-          totalPage={totalPage}
-          onPageChange={setCurrentPage}
-        />
-
-        <div className="functional-btns-container">
-          <ExportButton />
-          <ImportButton />
-        </div>
-      </Container>
-    </>
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map(row => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map(cell => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <div className="functional-btns-container">
+        <ExportButton />
+        <ImportButton />
+      </div>
+    </Container>
   );
 }
 
