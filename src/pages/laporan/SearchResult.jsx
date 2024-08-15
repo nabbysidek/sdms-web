64685bcbbbe0f796cd50053cdd2819e61050c86c
Nur Aina Balqis Mohamad Rosidek <nabby.sidek@gmail.com>
2024-08-15 15:10:39 +0800

    Update Laporan
    - Fetch audits data
    - Add sorting to the table
    - Add filter to the table
    - Add pagination

--- src/pages/laporan/SearchResult.jsx
+++ src/pages/laporan/SearchResult.jsx
@@ -1,70 +1,158 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import { useEffect, useMemo, useState } from "react";
+import { Container, Table } from "react-bootstrap";
 import ExportButton from "../../components/functional buttons/ExportBtn";
 import ImportButton from "../../components/functional buttons/ImportBtn";
-import Table from "react-bootstrap/Table";
+import Pagination from "../../components/page layout/Pagination";
+import useLaporanStore from "../../store/laporan-store";
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+  getPaginationRowModel,
+  getSortedRowModel,
+  getFilteredRowModel,
+} from "@tanstack/react-table";
+import { rankItem } from "@tanstack/match-sorter-utils";
+import "../../assets/styles/styles_laporan.css";
+
+// Filter component to handle different types of filters
+function Filter({ column }) {
+  const columnFilterValue = column.getFilterValue();
+  const { filterVariant } = column.columnDef.meta ?? {};
+
+  if (column.id === "index" || column.id === "tarikhAudit") {
+    return null;
+  }
+
+  return filterVariant === "select" ? (
+    <select
+      className="filter-bar"
+      onChange={(e) => column.setFilterValue(e.target.value)}
+      value={columnFilterValue || ""}
+    >
+      <option value="">Tiada Risiko</option>
+      <option value="BIASA">Biasa</option>
+      <option value="SERAH DOKUMEN">Serah Dokumen</option>
+      <option value="PENIPUAN">Penipuan</option>
+    </select>
+  ) : (
+    <input
+      type="text"
+      value={columnFilterValue || ""}
+      onChange={(e) => column.setFilterValue(e.target.value)}
+      placeholder={`Cari`}
+      className="filter-bar"
+    />
+  );
+}
 
 function SearchResultLaporan() {
-  return (
-    <>
-      <Container fluid>
-        <h4 className="page-title">
-          Senarai Laporan Ketidakpatuhan Kakitangan
-        </h4>
-        <hr />
-        <div className="dates-container">
-          <p>Hasil Carian: DD/MM/YYYY - DD/MM/YYYY</p>
-        </div>
-        <Table responsive>
-          <thead>
-            <tr>
-              <th>Bil</th>
-              <th>Tahun</th>
-              <th>Wilayah</th>
-              <th>Cawangan</th>
-              <th>Kesalahan Berulang</th>
-              <th>Jenis Audit</th>
-              <th>Skop Semakan</th>
-              <th>Skop Kriteria</th>
-              <th>Aktiviti Semakan</th>
-              <th>Kriteria Ketidakpatuhan</th>
-              <th>ID Kakitangan</th>
-              <th>Nama Kakitangan</th>
-              <th>Jawatan Kakitangan</th>
-              <th>Bahagian</th>
-              <th>Jabatan</th>
-              <th>Unit</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{/* Bilangan */}</td>
-              <td>{/* Tahun */}</td>
-              <td>{/* Wilayah */}</td>
-              <td>{/* Cawangan */}</td>
-              <td>{/* Sekalahan Berulang */}</td>
-              <td>{/* Jenis Audit */}</td>
-              <td>{/* Skop Semakan */}</td>
-              <td>{/* Skop Kriteria */}</td>
-              <td>{/* Aktiviti Semakan */}</td>
-              <td>{/* Kriteria Ketidakpatuhan */}</td>
-              <td>{/* ID Kakitangan */}</td>
-              <td>{/* Nama Kakitangan */}</td>
-              <td>{/* Jawatan Kakitangan */}</td>
-              <td>{/* Bahagian */}</td>
-              <td>{/* Jabatan */}</td>
-              <td>{/* Unit */}</td>
-            </tr>
-          </tbody>
-        </Table>
+  // USE OF LAPORAN STORE
+  const { audits, fetchAudits, columns } = useLaporanStore();
 
-        {/* Functional buttons */}
-        <div className="functional-btns-container">
-          <ExportButton />
-          <ImportButton />
+  // FETCH FROM STORE: AUDIT
+  useEffect(() => {
+    fetchAudits();
+  }, [fetchAudits]);
+
+  // FETCH DATA AND DECLARE COLUMNS
+  const data = useMemo(() => audits, [audits]);
+
+  // SORTING AND FILTERING
+  const [sorting, setSorting] = useState([]);
+  const [globalFilter, setGlobalFilter] = useState("");
+
+  // TABLE DECLARATION
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    state: { sorting, globalFilter },
+    onSortingChange: setSorting,
+    onGlobalFilterChange: setGlobalFilter,
+    filterFns: {
+      fuzzy: (row, columnId, value) => {
+        return rankItem(row.getValue(columnId), value).passed;
+      },
+    },
+  });
+
+  return (
+    <Container fluid>
+      <h4 className="page-title">Senarai Laporan Ketidakpatuhan Kakitangan</h4>
+      <hr />
+      <div className="dates-container">
+        <p>Hasil Carian: DD/MM/YYYY - DD/MM/YYYY</p>
+      </div>
+      <Table responsive>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                const isNarrowColumn =
+                  header.column.id === "index" ||
+                  header.column.id === "tarikhAudit";
+                return (
+                  <th
+                    key={header.id}
+                    className={isNarrowColumn ? "" : "wide-column"}
+                  >
+                    <div>
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                      {header.column.getCanSort() && (
+                        <span
+                          onClick={header.column.getToggleSortingHandler()}
+                          style={{ cursor: "pointer" }}
+                        >
+                          {header.column.getIsSorted() === "asc" && " 🔼"}
+                          {header.column.getIsSorted() === "desc" && " 🔽"}
+                        </span>
+                      )}
+                    </div>
+                    {header.column.getCanFilter() && (
+                      <Filter column={header.column} />
+                    )}
+                  </th>
+                );
+              })}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => {
+                const isNarrowColumn =
+                  cell.column.id === "index" ||
+                  cell.column.id === "tarikhAudit";
+                return (
+                  <td
+                    key={cell.id}
+                    className={isNarrowColumn ? "" : "wide-column"}
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <div className="pagination">
+          <Pagination table={table}/> 
         </div>
-      </Container>
-    </>
+        
+      <div className="functional-btns-container">
+        <ExportButton />
+        <ImportButton />
+      </div>
+    </Container>
   );
 }
 
---
+++ src/store/laporan-store.js
@@ -0,0 +1,93 @@
+import { create } from "zustand";
+import axiosCustom from "../axios";
+
+const useLaporanStore = create((set) => ({
+    audits: [],
+    columns: [
+        {
+          header: "Bil",
+          accessorFn: (row, i) => i + 1,
+          id: "index",
+        },
+        {
+          header: "Tarikh Audit",
+          accessorKey: "tarikhAudit",
+        },
+        {
+          header: "Tahap Risiko",
+          accessorKey: "tahapRisikoAudit",
+          meta: {
+            filterVariant: "select",
+          },
+        },
+        {
+          header: "Wilayah",
+          accessorKey: "wilayah.namaWilayah",
+        },
+        {
+          header: "Cawangan",
+          accessorKey: "cawangan.namaCawangan",
+        },
+        {
+          header: "Kesalahan Berulang",
+          accessorKey: "kesalahanBerulang",
+        },
+        {
+          header: "Jenis Audit",
+          accessorKey: "jenis_audit.namaJenisAudit",
+        },
+        {
+          header: "Skop Semakan",
+          accessorKey: "skop_semakan.namaSkopSemakan",
+        },
+        {
+          header: "Aktiviti Semakan",
+          accessorKey: "aktiviti_semakan.namaAktivitiSemakan",
+        },
+        {
+          header: "Kriteria Ketidakpatuhan",
+          accessorKey: "kriteria_ketidakpatuhan.namaKriteriaKetidakpatuhan",
+        },
+        {
+          header: "ID Kakitangan",
+          accessorKey: "kakitangan.idKakitangan",
+        },
+        {
+          header: "Nama Kakitangan",
+          accessorKey: "kakitangan.namaKakitangan",
+        },
+        {
+          header: "Jawatan Kakitangan",
+          accessorKey: "jawatanKakitangan",
+        },
+        {
+          header: "Bahagian",
+          accessorKey: "bahagian.namaBahagian",
+        },
+        {
+          header: "Jabatan",
+          accessorKey: "jabatan.namaJabatan",
+        },
+        {
+          header: "Unit",
+          accessorKey: "unit.namaUnit",
+        },
+      ],
+
+    // FETCH AUDITS FOR LAPORAN
+    fetchAudits: async () => {
+        try {
+            const response = await axiosCustom.get(
+                `/laporan/repot-individu`
+            );
+            set({
+                audits: response.data,
+            });
+          } catch (error) {
+            console.error("Ralat dalam mengambil maklumat audit:", error);
+          }
+    },
+
+}));
+
+export default useLaporanStore;
