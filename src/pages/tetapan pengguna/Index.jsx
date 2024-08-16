80836456c0648de667d44a8142814e8643a1d7d3
Nur Aina Balqis Mohamad Rosidek <nabby.sidek@gmail.com>
2024-08-16 11:44:50 +0800

    Update Index with setting peranan for pengguna

--- src/pages/tetapan pengguna/Index.jsx
+++ src/pages/tetapan pengguna/Index.jsx
@@ -6,18 +6,14 @@ import ModalRejectAccessPermohonanPengguna from "./ModalRejectAccessPermohonanPe
 import ModalTerminateAccessSenaraiPengguna from "./ModalTerminateAccessSenaraiPengguna";
 import "../../assets/styles/styles_tetapan_pengguna.css";
 import axiosCustom from "../../axios";
+import Swal from "sweetalert2";
 
 function IndexTetapanPengguna() {
-  // -------------------- BE ---------------------------
   // Fetch permohonan akses
   const [permohonanAkses, setPermohonanAkses] = useState([]);
-
   const fetchPermohonanAkses = async () => {
     try {
-      const response = await axiosCustom.get(
-        `/tetapan-pengguna/tetapan-akses-pengguna/permohonan-akses`
-      );
-
+      const response = await axiosCustom.get(`/tetapan-pengguna/tetapan-akses-pengguna/permohonan-akses`);
       if (response.status >= 200 && response.status < 300) {
         setPermohonanAkses(response.data);
       } else {
@@ -27,20 +23,15 @@ function IndexTetapanPengguna() {
       console.log(error);
     }
   };
-
   useEffect(() => {
     fetchPermohonanAkses();
   }, []);
 
   // Fetch senarai pengguna
   const [senaraiPengguna, setSenaraiPengguna] = useState([]);
-
   const fetchSenaraiPengguna = async () => {
     try {
-      const response = await axiosCustom.get(
-        `/tetapan-pengguna/tetapan-akses-pengguna/senarai-pengguna`
-      );
-
+      const response = await axiosCustom.get(`/tetapan-pengguna/tetapan-akses-pengguna/senarai-pengguna`);
       if (response.status >= 200 && response.status < 300) {
         setSenaraiPengguna(response.data);
       } else {
@@ -50,31 +41,25 @@ function IndexTetapanPengguna() {
       console.log(error);
     }
   };
-
   useEffect(() => {
     fetchSenaraiPengguna();
   }, []);
 
   // Fetch peranan for the dropdown
   const [perananOptions, setPerananOptions] = useState([]);
-
   const fetchPeranans = useCallback(async () => {
     try {
       const response = await axiosCustom.get(`/get-peranan`);
-
       if (Array.isArray(response.data)) {
-        setPerananOptions(
-          response.data.map((peranan) => ({
-            value: peranan.id,
-            label: peranan.namaPeranan,
-          }))
-        );
+        setPerananOptions(response.data.map((peranan) => ({
+          value: peranan.id,
+          label: peranan.namaPeranan,
+        })));
       }
     } catch (error) {
       console.log(error);
     }
   }, [setPerananOptions]);
-
   useEffect(() => {
     fetchPeranans();
   }, [fetchPeranans]);
@@ -85,19 +70,47 @@ function IndexTetapanPengguna() {
     return peranan ? peranan.label : 'Ralat';
   };
 
+  // Handle change in peranan
+  const handleChangePeranan = async (userId, perananId) => {
+    try {
+      const response = await axiosCustom.put(
+        `/tetapan-pengguna/senarai-pengguna/peranan/${userId}`,
+        { perananId }
+      );
+
+      if (response.status === 200) {
+        setSenaraiPengguna((prevUsers) =>
+          prevUsers.map((user) =>
+            user.id === userId ? { ...user, perananId } : user
+          )
+        );
+        Swal.fire({
+          icon: "success",
+          title: "Berjaya",
+          text: response.data.success, 
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Gagal",
+          text: error.response.data.error,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Terdapat ralat semasa mengemaskini peranan pengguna.");
+    }
+  };
+
   return (
     <>
-      {/* Page title section */}
       <div className="page-title">
         <h2>Tetapan Akses Pengguna</h2>
         <hr />
         <h3>Permohonan Akses</h3>
       </div>
-
-      {/* Page content section */}
       <Container fluid>
         <hr />
-        {/* Table Senarai Permohonan Akses */}
         <Table responsive>
           <thead>
             <tr>
@@ -123,12 +136,8 @@ function IndexTetapanPengguna() {
                   <td>{permohonanAksesData.namaAuditor}</td>
                   <td>{permohonanAksesData.emelAuditor}</td>
                   <td>
-                    <ModalAllowAccessPermohonanPengguna
-                      userId={permohonanAksesData.id}
-                    />
-                    <ModalRejectAccessPermohonanPengguna
-                      userId={permohonanAksesData.id}
-                    />
+                    <ModalAllowAccessPermohonanPengguna userId={permohonanAksesData.id} />
+                    <ModalRejectAccessPermohonanPengguna userId={permohonanAksesData.id} />
                   </td>
                 </tr>
               ))
@@ -138,7 +147,6 @@ function IndexTetapanPengguna() {
 
         <h4 className="page-title">Senarai Pengguna</h4>
         <hr />
-        {/* Table Senarai Permohonan Akses */}
         <Table responsive>
           <thead>
             <tr>
@@ -152,7 +160,7 @@ function IndexTetapanPengguna() {
             </tr>
           </thead>
           <tbody>
-            {senaraiPengguna.length < 0 ? (
+            {senaraiPengguna.length === 0 ? (
               <tr>
                 <td colSpan="7">
                   <center>Tiada rekod.</center>
@@ -167,19 +175,6 @@ function IndexTetapanPengguna() {
                   <td>{senaraiPenggunaData.emelAuditor}</td>
                   <td>{senaraiPenggunaData.statusAuditor}</td>
                   <td>
-                    {/* <Dropdown>
-                      <Dropdown.Toggle className="user-level-btn">
-                        Tahap Pengguna
-                      </Dropdown.Toggle>
-
-                      <Dropdown.Menu className="user-level-item">
-                        {perananOptions.map((perananOptions) => (
-                          <Dropdown.Item key={perananOptions.value}>
-                            {perananOptions.label}
-                          </Dropdown.Item>
-                        ))}
-                      </Dropdown.Menu>
-                    </Dropdown> */}
                     <Dropdown>
                       <Dropdown.Toggle className="user-level-btn">
                         {getPerananLabel(senaraiPenggunaData.perananId)}
@@ -187,7 +182,10 @@ function IndexTetapanPengguna() {
 
                       <Dropdown.Menu className="user-level-item">
                         {perananOptions.map((perananOption) => (
-                          <Dropdown.Item key={perananOption.value}>
+                          <Dropdown.Item 
+                            key={perananOption.value} 
+                            onClick={() => handleChangePeranan(senaraiPenggunaData.id, perananOption.value)}
+                          >
                             {perananOption.label}
                           </Dropdown.Item>
                         ))}
@@ -196,15 +194,11 @@ function IndexTetapanPengguna() {
                   </td>
                   <td>
                     <ModalAllowAccessSenaraiPengguna
-                      disableButtonBenar={
-                        senaraiPenggunaData.statusAuditor === "BENAR"
-                      }
+                      disableButtonBenar={senaraiPenggunaData.statusAuditor === "BENAR"}
                       userId={senaraiPenggunaData.id}
                     />
                     <ModalTerminateAccessSenaraiPengguna
-                      disableButtonSekat={
-                        senaraiPenggunaData.statusAuditor === "SEKAT"
-                      }
+                      disableButtonSekat={senaraiPenggunaData.statusAuditor === "SEKAT"}
                       userId={senaraiPenggunaData.id}
                     />
                   </td>
