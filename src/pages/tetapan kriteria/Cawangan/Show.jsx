29cd5558ae183c1f7a7c58bfe72409ae3c8f627b
Nur Aina Balqis Mohamad Rosidek <nabby.sidek@gmail.com>
2024-08-23 11:55:21 +0800

    Update Show.jsx
    Use the TableComponent

--- src/pages/tetapan kriteria/Cawangan/Show.jsx
+++ src/pages/tetapan kriteria/Cawangan/Show.jsx
@@ -1,21 +1,13 @@
 import { useState, useEffect, useMemo, useCallback } from "react";
-import { Table, Row, Button, Container } from "react-bootstrap";
+import { Row, Button, Container } from "react-bootstrap";
 import CreateCawangan from "./Create";
 import EditCawangan from "./Edit";
 import SearchCawangan from "./Search";
 import showConfirmationDialog from "../showConfirmationDialog";
-import Pagination from "../../../components/page layout/Pagination";
 import ExportButton from "../../../components/functional buttons/ExportBtn";
 import ImportButton from "../../../components/functional buttons/ImportBtn";
 import useCawanganStore from "../../../store/cawangan-store";
-import {
-  useReactTable,
-  getCoreRowModel,
-  flexRender,
-  getPaginationRowModel,
-  getSortedRowModel,
-  getFilteredRowModel,
-} from "@tanstack/react-table";
+import TableComponent from "../../../components/TableComponent";
 
 function ShowCawanganList() {
   // USE OF CAWANGAN STORE
@@ -87,19 +79,6 @@ function ShowCawanganList() {
   const [sorting, setSorting] = useState([]);
   const [filtering, setFiltering] = useState("");
 
-  // TABLE DECLARATION
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    state: { sorting: sorting, globalFilter: filtering },
-    onSortingChange: setSorting,
-    onGlobalFilterChange: setFiltering,
-  });
-
   return (
     <>
       <Container fluid>
@@ -118,43 +97,14 @@ function ShowCawanganList() {
           </Row>
         </div>
         <hr />
-        <Table responsive>
-          <thead>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <th
-                    key={header.id}
-                    onClick={header.column.getToggleSortingHandler()}
-                  >
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-                    {
-                      { asc: " 🔼", desc: " 🔽" }[
-                        header.column.getIsSorted() ?? null
-                      ]
-                    }
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody>
-            {table.getRowModel().rows.map((row) => (
-              <tr key={row.id}>
-                {row.getVisibleCells().map((cell) => (
-                  <td key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-        {/* PAGINATION */}
-        <Pagination table={table}/>
+        <TableComponent
+        data={data}
+        columns={columns}
+        sorting={sorting}
+        setSorting={setSorting}
+        filtering={filtering}
+        setFiltering={setFiltering}
+      />
         
         {/* IMPORT AND EXPORT */}
         <div className="functional-btns-container">
