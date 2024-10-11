446e5004861535735cb79595ea0ca5964b5775af
Nur Aina Balqis Mohamad Rosidek <nabby.sidek@gmail.com>
2024-10-11 10:37:51 +0800

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Axios instance
 const axiosCustom = axios.create({
-  baseURL: "http://localhost:5173/",
+  baseURL: "http://127.0.0.1:8000/api/",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
